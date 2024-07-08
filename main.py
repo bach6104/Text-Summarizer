30bb192a42e14f6141ab5a60c05674d5ae033abe
Admin <nguy1104@msu.edu>
2024-07-08 13:26:07 +0900

    Finished model trainer and evaluation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from textSummarizer.logs import logger
 import os 
 
@@ -34,6 +35,17 @@ try:
     data_transformation.main()
     logger.info(f"Completed {STAGE_NAME}")
 
+except Exception as e:
+    logger.error(f"Failed to execute {STAGE_NAME} pipeline")
+    logger.error(str(e))
+
+STAGE_NAME = "Model Training Stage"
+
+try:
+    logger.info(f"Starting {STAGE_NAME}")
+    model_training = ModelTrainerTrainingPipeline()
+    model_training.main()
+    logger.info(f"Completed {STAGE_NAME}")
 except Exception as e:
     logger.error(f"Failed to execute {STAGE_NAME} pipeline")
     logger.error(str(e))
