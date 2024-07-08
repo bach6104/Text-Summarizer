a5cb749343dae33cb5b6e165227994706637ad5b
Admin <nguy1104@msu.edu>
2024-07-08 10:48:57 +0900

    finish data validation stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.logs import logger
 import os 
 
@@ -21,6 +22,18 @@ try:
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
     logger.info(f"Completed {STAGE_NAME}")
+except Exception as e:
+    logger.error(f"Failed to execute {STAGE_NAME} pipeline")
+    logger.error(str(e))
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+    logger.info(f"Starting {STAGE_NAME}")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f"Completed {STAGE_NAME}")
+
 except Exception as e:
     logger.error(f"Failed to execute {STAGE_NAME} pipeline")
     logger.error(str(e))
